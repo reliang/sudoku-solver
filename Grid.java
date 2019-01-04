079b48dcd93f7b992e1777f1298e5b5efc8873b0
Catherine Liang <46097972+reliang@users.noreply.github.com>
2019-01-03 20:34:04 -0500

    Add files via upload

---
+++ Grid.java
@@ -0,0 +1,253 @@
+/*  Name: Reshu Catherine Liang
+ *  PennKey: reliang
+ *  Recitation: 219
+ *
+ *  Execution: Grid g = new Grid(String file);
+ * 
+ *  Creates a grid using a Sudoku textfile
+ *
+ */
+
+public class Grid {
+    private String file;
+    private int[][] grid;
+    private int size = 6;
+    private int currentRow = -1;
+    private int currentCol = -1;
+    
+    // contructor
+    // creates a Sudoku grid with file
+    public Grid(String file) {
+        In inStream = new In(file);
+        this.file = file;
+        
+        if (this.getRowCount() != size) {
+            throw new RuntimeException("ERROR: Row count does not equal " +
+                                       size);
+        }
+        for (int i = 0; i < this.getRowCount(); i++) {
+            if (this.getColumnCount(i) != size) {
+                throw new RuntimeException("ERROR: Column count does not " + 
+                                           "equal " + size);
+            }
+        }
+        
+        grid = new int[size][size];
+        // goes through every row and reads the line using inStream
+        for (int i = 0; i < size; i++) {
+            String str = inStream.readLine();
+            // goes through every character in the line and stores it in array
+            for (int j = 0; j < size; j++) {
+                // space is converted into 0
+                if (str.charAt(j) == ' ') {
+                    grid[i][j] = 0;
+                } else if (str.charAt(j) >= '1' && 
+                           str.charAt(i) <= '6') {
+                    grid[i][j] = str.charAt(j) - '0';
+                } else {
+                    throw new RuntimeException("ERROR: Sudoku contains " +
+                                               "invalid characters");
+                }
+            }
+        }
+    }
+    
+    // returns row count
+    public int getRowCount() {
+        In inStream = new In(file);
+        String str = inStream.readAll();
+        int rowCount = 0;
+        // counts row number by the number of '\n'
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '\n') {
+                rowCount += 1;
+            }
+        }
+        // in case the last row doesn't end with '\n'
+        if (str.charAt(str.length() - 1) != '\n') {
+            rowCount += 1;
+        }
+        return rowCount;
+    }
+    
+    // returns the column count of one row
+    public int getColumnCount(int row) {
+        In inStream = new In(file);
+        String str = "";
+        for (int i = 0; i <= row; i++) {
+            str = inStream.readLine();
+        }
+        return str.length();
+    }
+    
+    // returns a specified row in an array
+    public int[] row(int i) {
+        if (i < 0 || i >= size) {
+            throw new IllegalArgumentException("ERROR: row number is invalid");
+        }
+        int[] arr = new int[size];
+        for (int j = 0; j < size; j++) {
+            arr[j] = grid[i][j];
+        }
+        return arr;
+    }
+    
+    // returns the current row
+    public int[] row() {
+        return row(currentRow);
+    }
+    
+    // returns a specified column in an array
+    public int[] col(int j) {
+        if (j < 0 || j >= size) {
+            throw new IllegalArgumentException("ERROR: col number is invalid");
+        }
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = grid[i][j];
+        }
+        return arr;
+    }
+    
+    // returns the current column
+    public int[] col() {
+        return col(currentCol);
+    }
+    
+    // returns the 2 by 3 box in an array
+    public int[] twoByThree(int i, int j) {
+        int[] arr = new int[size];
+        int[] rows;
+        int[] columns;
+        if (i == 0 || i == 1) {
+            int[] x = {0, 1};
+            rows = x;
+        } else if (i == 2 || i == 3) {
+            int[] x = {2, 3};
+            rows = x;
+        } else if (i == 4 || i == 5) {
+            int[] x = {4, 5};
+            rows = x;
+        // is it none of these rows?
+        } else {
+            throw new IllegalArgumentException("ERROR: row number is " +
+                                               "invalid");
+        }
+        if (j >= 0 && j <= 2) {
+            int[] y = {0, 1, 2}; 
+            columns = y;
+        } else if (j >= 3 && j <= 5) {
+            int[] y = {3, 4, 5};
+            columns = y;
+        } else {
+            throw new IllegalArgumentException("ERROR: col number is " +
+                                               "invalid");
+        }
+        for (int index = 0; index < 3; index++) {
+            arr[index] = grid[rows[0]][columns[index]];
+            arr[index + 3] = grid[rows[1]][columns[index]];
+        }
+        return arr;
+    }
+    
+    // returns the current twoByThree
+    public int[] twoByThree() {
+        return twoByThree(currentRow, currentCol);
+    }
+    
+    // prints the 2D array
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] == 0) {
+                    str += " ";
+                } else {
+                    str += grid[i][j];
+                }
+            }
+            str += "\n";
+        }
+        return str;
+    }
+    
+    // goes to the next empty box
+    public void nextEmptyBox() {
+        // if there isn't a current box, start with the first box
+        if (currentRow == -1) {
+            search(0, 0);
+            return;
+        }
+        // if there is a current row and column, start there
+        // if it succeeds, return
+        if (search(currentRow, currentCol + 1)) {
+            return;
+        }
+        // if it fails, restart from beginning
+        if (search(0, 0)) {
+            return;
+        }
+        // if it fails again, set currentRow to -1 to show grid is full
+        currentRow = -1;
+    }
+    
+    // helper function for nextEmptyBox()
+    // takes in where to start search, returns true if next empty box is found
+    private boolean search(int row, int column) {
+        // for the row of the given box, only consider what is right of box
+        int i = row;
+        for (int j = column; j < size; j++) {
+            if (grid[i][j] == 0) {
+                currentRow = i;
+                currentCol = j;
+                return true;
+            }
+        }
+        // for the rest of the rows, box can be any column
+        for (i = row + 1; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] == 0) {
+                    currentRow = i;
+                    currentCol = j;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    // gets value of currentRow
+    public int getCurrentRow() {
+        return currentRow;
+    }
+    
+    public int getCurrentCol() {
+        return currentCol;
+    }
+    
+    // sets value of current box
+    public void set(int i) {
+        grid[currentRow][currentCol] = i;
+    }
+    
+    // gets the grid size
+    public int getSize() {
+        return size;
+    }
+    
+    public static void main(String[] args) {
+        // Usage: java Grid sudokuExample.txt
+        Grid x = new Grid(args[0]);
+        System.out.println(x.toString());
+        System.out.println(x.getRowCount());
+        int[] arr = x.twoByThree(2, 4);
+        for (int i = 0; i < 6; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < 30; i++) {
+            x.nextEmptyBox();
+            System.out.println(x.currentRow + ", " + x.currentCol);
+        }
+    }
+}
---
+++ SudokuSolver.java
@@ -0,0 +1,132 @@
+/*  Name: Reshu Catherine Liang
+ *  PennKey: reliang
+ *  Recitation: 219
+ *
+ *  Execution: java SudokuSolver sudokuExample.txt
+ * 
+ *  Solves 6x6 Sudokus
+ *
+ */
+
+import java.util.ArrayList;
+
+public class SudokuSolver {
+    
+    // checks if Sudoku is valid by going through each row, column and 2-by-3
+    public void check(Grid grid) {
+        for (int i = 0; i < grid.getSize(); i++) {
+            checkRepeats(grid.row(i));
+            checkRepeats(grid.col(i));
+            // goes through top left box, the two middle ones, and bottom right
+            checkRepeats(grid.twoByThree(i, i));
+            // goes through top right, the two middle ones, and bottom left
+            checkRepeats(grid.twoByThree(i, 5 - i));
+        }
+    }
+    
+    // checks for repeats in an array of 6 numbers
+    public void checkRepeats(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] != 0 && arr[j] != 0 && arr[i] == arr[j]) {
+                    throw new RuntimeException("ERROR: Input sudoku is " + 
+                                               "not valid");
+                }
+            }
+        }
+    }
+        
+    // resets the possibleSolutions array and fills it with numbers 1-6
+    public void resetSolutions(ArrayList<Integer> solutions) {
+        solutions.clear();
+        for (int i = 1; i < 7; i++) {
+            solutions.add(i);
+        }
+    }
+    
+    // does process of elimination to find possible solution
+    public void poe(int[] arr, ArrayList<Integer> solutions) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < solutions.size(); j++) {
+                if (arr[i] == solutions.get(j)) {
+                    solutions.remove(j);
+                }
+            }
+        }
+    }
+    
+    // goes through each blank and solves Sudoku
+    // returns true if succeeds
+    public boolean solve(Grid grid) {
+        grid.nextEmptyBox();
+        // marks the first empty box
+        int markRow = grid.getCurrentRow();
+        int markCol = grid.getCurrentCol();
+        while (grid.getCurrentRow() != -1) {            
+            // if a solution is found, record location of next empty box
+            if (findSolution(grid)) {
+                grid.nextEmptyBox();
+                markRow = grid.getCurrentRow();
+                markCol = grid.getCurrentCol();
+            // if a solution isn't found, go to next empty box
+            } else {
+                grid.nextEmptyBox();
+                // if it's been a whole round and we still don't find a solution
+                // Sudoku cannot be solved by simple logic
+                if (markRow == grid.getCurrentRow() && 
+                    markCol == grid.getCurrentCol()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    // allows you to see the grid after a certain number of steps
+    public void step(Grid grid, int step) {
+        for (int i = 0; i < step; i++) {
+            grid.nextEmptyBox();
+            findSolution(grid);
+        }
+    }
+    
+    // tries process of elimination, fills in solution if succeeds
+    // returns true if a solution is found
+    public boolean findSolution(Grid grid) {
+        ArrayList<Integer> solutions = new ArrayList<Integer>();
+        resetSolutions(solutions);
+        poe(grid.row(), solutions);
+        poe(grid.col(), solutions);
+        poe(grid.twoByThree(), solutions);
+        if (solutions.size() == 1) {
+            int solution = solutions.get(0);
+            grid.set(solution);
+            return true;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        // Usage: java SudokuSolver sudokuExample.txt
+        Grid grid = new Grid(args[0]);
+        SudokuSolver s = new SudokuSolver();
+        // checks if input Sudoku is valid
+        s.check(grid);
+        System.out.println(grid.toString());
+        
+        // prints grid after a specific number of steps
+        /*
+        s.step(grid, 30);
+        System.out.println(grid.toString());
+        */
+        
+        // prints grid and reveals if solver succeeded or failed
+        if (s.solve(grid)) {
+            System.out.println("---DONE---");
+        } else {
+            System.out.println("---FAILED---");
+        }
+        System.out.println();
+        System.out.println(grid.toString());
+    }
+}
